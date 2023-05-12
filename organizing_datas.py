02ec1d98db77244492fc12fa77e2656f89633b79
MUNKHERDENE OTGONBAYAR <MUNKH.HUSTLE@gmail.com>
2023-05-12 23:44:26 +0800

    idk

--- organizing_datas.py
+++ organizing_datas.py
@@ -1,8 +1,11 @@
 import csv
+import numpy as np
 
 # 22_s1_les.csv
 with open('22_s2_les1.csv', newline='', encoding="utf8") as f:
     reader = csv.reader(f)
     data = list(reader)
 
-print(data)
+# print(data)
+array1 = np.array(data)
+print(array1.dtype) # shape (row, column)
