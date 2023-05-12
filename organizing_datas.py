a428a03e8e081212d6e45144d0bbfca78c8ad21f
MUNKHERDENE OTGONBAYAR <MUNKH.HUSTLE@gmail.com>
2023-05-13 06:52:20 +0800

    how?

--- organizing_datas.py
+++ organizing_datas.py
@@ -10,5 +10,18 @@ with open('22_s2_les1.csv', newline='', encoding="utf8") as f:
 array1 = np.array(data)
 print(array1.dtype) # shape (row, column)
 
-# goal:
-# horizontal values to vertical
+# to do:
+# horizontal values to vertical ?
+
+# r.n my data:
+# id - 0,0,0
+# name - a,a,a
+# gmail - @,@,@
+# num1 - 1,1,1
+# num2 - 2,2,2
+
+# wanted data:
+# id, name, gmail, num1, num2
+# 0,a,@,1,2
+# 0,a,@,1,2
+
