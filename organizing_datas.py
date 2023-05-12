2cb0e3f286653bf11573444f55ff0ddaea984c3c
MUNKHERDENE OTGONBAYAR <MUNKH.HUSTLE@gmail.com>
2023-05-12 23:45:20 +0800

    idk

--- organizing_datas.py
+++ organizing_datas.py
@@ -9,3 +9,6 @@ with open('22_s2_les1.csv', newline='', encoding="utf8") as f:
 # print(data)
 array1 = np.array(data)
 print(array1.dtype) # shape (row, column)
+
+# goal:
+# horizontal values to vertical
