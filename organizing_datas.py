751a36004f33865ac1a514e29b4722027ecc7588
MUNKHERDENE OTGONBAYAR <MUNKH.HUSTLE@gmail.com>
2023-05-13 17:53:55 +0800

    idk

--- organizing_datas.py
+++ organizing_datas.py
@@ -1,66 +1,98 @@
 import csv
 import numpy as np
 
-# 22_s1_les.csv
-# with open('22_s2_les1.csv', newline='', encoding="utf8") as f:
-#     reader = csv.reader(f)
-#     data = list(reader)
-
-
 # to do:
 # horizontal values to vertical ?
 
-id_line = []
-name_line = []
-gmail_line = []
-number1_line = []
-number2_line = []
+file_name_no_csv = "test_file"
 
-with open('test_file.csv',encoding="utf8") as csv_file: 
-    data = csv.reader(csv_file)
+def get_np_id_from_file(file_name):
+    id_list = []
 
-    # taking id line
-    for id in list(data)[0:1]:
-        id_line.append(id)
+    with open(str(file_name)+'.csv',encoding="utf8") as csv_file: 
+        data = csv.reader(csv_file)
 
-with open('test_file.csv',encoding="utf8") as csv_file: 
-    data = csv.reader(csv_file)
+        for id in list(data)[0:1]:
+            id_list.append(id)
+    
+    np_id = np.array(id_list)
 
-    # taking name line
-    for name in list(data)[1:2]:
-        name_line.append(name)
+    return np_id
 
-with open('test_file.csv',encoding="utf8") as csv_file: 
-    data = csv.reader(csv_file)
+def get_np_name_from_file(file_name):
 
-    # taking name line
-    for gmail in list(data)[2:3]:
-        gmail_line.append(gmail)
+    name_list = []
 
-with open('test_file.csv',encoding="utf8") as csv_file: 
-    data = csv.reader(csv_file)
+    with open(str(file_name)+'.csv',encoding="utf8") as csv_file: 
+        data = csv.reader(csv_file)
 
-    # taking name line
-    for number1 in list(data)[3:4]:
-        number1_line.append(number1)
+        for name in list(data)[1:2]:
+            name_list.append(name)
 
-with open('test_file.csv',encoding="utf8") as csv_file: 
-    data = csv.reader(csv_file)
+    np_name = np.array(name_list)
 
-    # taking name line
-    for number2 in list(data)[4:]:
-        number2_line.append(number2)
+    return np_name
 
-# convert list to np array
-np_id_line = np.array(id_line)
-np_name_line = np.array(name_line)
-np_gmail_line = np.array(gmail_line)
-np_number1_line = np.array(number1_line)
-np_number2_line = np.array(number2_line)
+def get_np_gmail_from_file(file_name):
 
-print("\n id line\n",np_id_line)
-print("\n name line\n",np_name_line)
+    gmail_list = []
 
-np_new_line = np.dstack((np_id_line, np_name_line,np_gmail_line,np_number1_line,np_number2_line))
-print("\n new list\n",np_new_line.shape)
+    with open(str(file_name)+'.csv',encoding="utf8") as csv_file: 
+            data = csv.reader(csv_file)
 
+            for gmail in list(data)[2:3]:
+                gmail_list.append(gmail)
+
+    np_gmail = np.array(gmail_list)
+    
+    return np_gmail
+
+def get_np_number1_from_file(file_name):
+
+    number1_list = []
+
+    with open(str(file_name)+'.csv',encoding="utf8") as csv_file: 
+            data = csv.reader(csv_file)
+
+            for number1 in list(data)[3:4]:
+                number1_list.append(number1)
+
+    np_number1 = np.array(number1_list)
+
+    return np_number1
+
+def get_np_number2_from_file(file_name):
+    
+    number2_list = []
+
+    with open(str(file_name)+'.csv',encoding="utf8") as csv_file: 
+            data = csv.reader(csv_file)
+
+            for number2 in list(data)[4:]:
+                number2_list.append(number2)
+    
+    np_number2 = np.array(number2_list)
+
+    return np_number2
+
+def convert_np_all_array_to_list(file_name):
+
+    id_list = [] 
+    name_list = [] 
+    gmail_list = []
+    number1_list = [] 
+    number2_list = [] 
+
+    id_list = get_np_id_from_file(file_name)
+    name_list = get_np_name_from_file(file_name)
+    gmail_list = get_np_gmail_from_file(file_name)
+    number1_list = get_np_number1_from_file(file_name)
+    number2_list = get_np_number2_from_file(file_name)
+
+    new_list = np.dstack((id_list, name_list,gmail_list,number1_list,number2_list))
+    
+    return new_list
+
+all_data = convert_np_all_array_to_list(file_name_no_csv)
+
+print(all_data.size)
