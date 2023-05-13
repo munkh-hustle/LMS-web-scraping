dd6e0f7b1235509cb7e5f9eea65e0efb9714ac6c
MUNKHERDENE OTGONBAYAR <MUNKH.HUSTLE@gmail.com>
2023-05-13 15:15:02 +0800

    idk

--- organizing_datas.py
+++ organizing_datas.py
@@ -2,26 +2,65 @@ import csv
 import numpy as np
 
 # 22_s1_les.csv
-with open('22_s2_les1.csv', newline='', encoding="utf8") as f:
-    reader = csv.reader(f)
-    data = list(reader)
+# with open('22_s2_les1.csv', newline='', encoding="utf8") as f:
+#     reader = csv.reader(f)
+#     data = list(reader)
 
-# print(data)
-array1 = np.array(data)
-print(array1.dtype) # shape (row, column)
 
 # to do:
 # horizontal values to vertical ?
 
-# r.n my data:
-# id - 0,0,0
-# name - a,a,a
-# gmail - @,@,@
-# num1 - 1,1,1
-# num2 - 2,2,2
+id_line = []
+name_line = []
+gmail_line = []
+number1_line = []
+number2_line = []
 
-# wanted data:
-# id, name, gmail, num1, num2
-# 0,a,@,1,2
-# 0,a,@,1,2
+with open('test_file.csv',encoding="utf8") as csv_file: 
+    data = csv.reader(csv_file)
+
+    # taking id line
+    for id in list(data)[0:1]:
+        id_line.append(id)
+
+with open('test_file.csv',encoding="utf8") as csv_file: 
+    data = csv.reader(csv_file)
+
+    # taking name line
+    for name in list(data)[1:2]:
+        name_line.append(name)
+
+with open('test_file.csv',encoding="utf8") as csv_file: 
+    data = csv.reader(csv_file)
+
+    # taking name line
+    for gmail in list(data)[2:3]:
+        gmail_line.append(gmail)
+
+with open('test_file.csv',encoding="utf8") as csv_file: 
+    data = csv.reader(csv_file)
+
+    # taking name line
+    for number1 in list(data)[3:4]:
+        number1_line.append(number1)
+
+with open('test_file.csv',encoding="utf8") as csv_file: 
+    data = csv.reader(csv_file)
+
+    # taking name line
+    for number2 in list(data)[4:]:
+        number2_line.append(number2)
+
+# convert list to np array
+np_id_line = np.array(id_line)
+np_name_line = np.array(name_line)
+np_gmail_line = np.array(gmail_line)
+np_number1_line = np.array(number1_line)
+np_number2_line = np.array(number2_line)
+
+print("\n id line\n",np_id_line)
+print("\n name line\n",np_name_line)
+
+np_new_line = np.dstack((np_id_line, np_name_line,np_gmail_line,np_number1_line,np_number2_line))
+print("\n new list\n",np_new_line.shape)
 
